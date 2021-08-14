3c319eeeed4ceb57206dd6be19e50c9aa9eda612
João Pedro A. Freitas <pedrofreit4s12@gmail.com>
2021-08-14 01:16:26 -0300

    api

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -5,7 +5,7 @@ export function getApiClient(ctx?: any) {
   const { 'sirus.session': token } = parseCookies(ctx)
 
   const api = axios.create({
-    baseURL: 'http://localhost:3333/v1',
+    baseURL: 'https://b24461cafa37.ngrok.io/v1',
   })
 
   if (token) {
